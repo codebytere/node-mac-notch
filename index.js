ea75729ded12e17a91c1cfb3145e1d0bef5273a7
Shelley Vohr <shelley.vohr@gmail.com>
2021-11-11 16:10:36 +0100

    fix: add missing bindings

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const notch = require('bindings')('notch.node')
 
 module.exports = {
+  getAllDisplays: notch.getAllDisplays,
+  getDisplayById: notch.getDisplayById,
   safeAreaInsets: notch.safeAreaInsets,
   auxiliaryTopLeftArea: notch.auxiliaryTopLeftArea,
   auxiliaryTopRightArea: notch.auxiliaryTopRightArea,
